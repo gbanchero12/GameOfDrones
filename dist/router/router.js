d540444f3384b4a19717c3a39701a5890d2700bf
t_bancg <guillermo.banchero@autodesk.com>
2019-08-29 09:41:09 -0300

    id added

--- dist/router/router.js
+++ dist/router/router.js
@@ -2,10 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const router = express_1.Router();
-router.get('/gamble', (req, res) => {
+router.get('/gamble/:id', (req, res) => {
+    const id = req.params.id;
     res.json({
         ok: true,
-        mensaje: 'Todo esta bien!'
+        mensaje: 'Todo esta bien!',
+        id
     });
 });
 exports.default = router;
--- src/router/router.ts
+++ src/router/router.ts
@@ -2,10 +2,14 @@ import { Router, Request, Response } from 'express';
 
 const router = Router();
 
-router.get('/gamble', (req: Request, res: Response)=> {
+router.get('/gamble/:id', (req: Request, res: Response)=> {
+
+    const id = req.params.id;
+
     res.json({
         ok: true,
-        mensaje: 'Todo esta bien!'
+        mensaje: 'Todo esta bien!',
+        id
     })
 })
 
